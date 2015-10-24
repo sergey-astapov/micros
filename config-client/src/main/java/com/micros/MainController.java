16d7cb49dffec0822ad14e6455bac87fb87a1676
sergey.astapov <serg.astapov@gmail.com>
2015-10-25 02:41:24 +0300

    - add client

---
+++ config-client/src/main/java/com/micros/MainController.java
@@ -0,0 +1,15 @@
+package com.micros;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @Value("${property.1}") String property1;
+
+    @RequestMapping("/property1")
+    public String showLuckyWord() {
+        return "The property1 is: " + property1;
+    }
+}
