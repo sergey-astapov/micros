36ca5c59793d5ca76d1419ad1bc8324030cbcc0a
sergey.astapov <serg.astapov@gmail.com>
2015-11-01 01:57:57 +0300

    - add load balancer

--- discovery-client/src/main/java/com/micros/MainController.java
+++ discovery-client/src/main/java/com/micros/MainController.java
@@ -2,33 +2,26 @@ package com.micros;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-import java.util.List;
-
 @RestController
 public class MainController {
     @Autowired
-    DiscoveryClient client;
+    LoadBalancerClient client;
 
     @RequestMapping("/sentence")
     public @ResponseBody String getSentence() {
-        return getWord("discoverable-service") + ".";
+        return getWord("discoverable-service-subject") + "." +
+                getWord("discoverable-service-noun") + "." +
+                getWord("discoverable-service-verb") + ".";
     }
 
     public String getWord(String service) {
-        List<ServiceInstance> list = client.getInstances(service);
-        if (list != null && list.size() > 0 ) {
-            URI uri = list.get(0).getUri();
-            if (uri !=null ) {
-                return (new RestTemplate()).getForObject(uri,String.class);
-            }
-        }
-        return null;
+        ServiceInstance i = client.choose(service);
+        return i != null ? (new RestTemplate()).getForObject(i.getUri(), String.class) : null;
     }
 }
