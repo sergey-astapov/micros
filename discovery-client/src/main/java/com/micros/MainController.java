529b587c2c4499b6f644d01dad0ed3677249bd5d
sergey.astapov <serg.astapov@gmail.com>
2015-10-31 23:58:06 +0300

    - add discovery server, service and client

---
+++ discovery-client/src/main/java/com/micros/MainController.java
@@ -0,0 +1,34 @@
+package com.micros;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class MainController {
+    @Autowired
+    DiscoveryClient client;
+
+    @RequestMapping("/sentence")
+    public @ResponseBody String getSentence() {
+        return getWord("discoverable-service") + ".";
+    }
+
+    public String getWord(String service) {
+        List<ServiceInstance> list = client.getInstances(service);
+        if (list != null && list.size() > 0 ) {
+            URI uri = list.get(0).getUri();
+            if (uri !=null ) {
+                return (new RestTemplate()).getForObject(uri,String.class);
+            }
+        }
+        return null;
+    }
+}
